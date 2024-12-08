eafa571dae68f53fe69a514255cc6522fcacb649
Gokul Raj V <gokulrajv108@gmail.com>
2024-12-08 12:29:55 +0530

    Making tags case insensitive and removing private tags

--- libbm/service.py
+++ libbm/service.py
@@ -13,11 +13,15 @@ class Service:
             self.__db_engine = DBEngine(db_file, db_lock)
             self.__db_engine.create_tables()
 
-    def create_site(self, site_name: str, site_url: str, tags: list[str]):
-        site_id = self.__db_engine.insert_site(site_name, site_url)
-        for tag in tags:
+    def __update_tags(self, site_id: str, tags: list[str]):
+        case_insensitive_tags = [tag.lower() for tag in tags]
+        for tag in case_insensitive_tags:
             tag_id = self.__db_engine.get_tag_id(tag)
             self.__db_engine.map_site_tag(site_id, tag_id)
+
+    def create_site(self, site_name: str, site_url: str, tags: list[str]):
+        site_id = self.__db_engine.insert_site(site_name, site_url)
+        self.__update_tags(site_id, tags)
 
     def read_site(self, site_id: str) -> tuple:
         site_data = self.__db_engine.get_site(site_id)
@@ -26,7 +30,8 @@ class Service:
         return *site_data, " ".join(site_tags)
 
     def read_sites(self, tags: list[str]) -> list[tuple]:
-        sites = self.__db_engine.get_sites(tags)
+        case_insensitive_tags = [tag.lower() for tag in tags]
+        sites = self.__db_engine.get_sites(case_insensitive_tags)
         return sites
 
     def update_site(self, site_id: str, site_name: str, site_url: str, tags: list[str]):
@@ -35,30 +40,15 @@ class Service:
             return
 
         self.__db_engine.delete_tags_map(site_id)
-        for tag in tags:
-            tag_id = self.__db_engine.get_tag_id(tag)
-            self.__db_engine.map_site_tag(site_id, tag_id)
+        self.__update_tags(site_id, tags)
 
     def delete_site(self, site_id: str):
         self.__db_engine.delete_tags_map(site_id)
         self.__db_engine.delete_site(site_id)
 
     def read_tags(self):
-        tags = self.__db_engine.get_tags()
-
-        private_tags = []
-        public_tags = []
-        for tag in tags:
-            if tag.startswith("."):
-                private_tags.append(tag)
-            else:
-                public_tags.append(tag)
-
-        # sorting tags alphabetically
-        private_tags.sort()
-        public_tags.sort()
-
-        return public_tags + private_tags
+        tags = self.__db_engine.get_tags().sort()
+        return tags
 
     def close(self):
         self.__db_engine.close()
