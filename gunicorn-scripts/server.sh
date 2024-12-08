5255cd1e7d15933acc66aee1d93d55b98059ef13
Gokul Raj V <gokulrajv108@gmail.com>
2024-12-08 12:15:39 +0530

    Changing start-stop script for gunicorn server

---
+++ gunicorn-scripts/server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+case $1 in
+    start)
+        if pgrep gunicorn > /dev/null 2>&1; then
+            echo 'Already gunicorn server running'
+        else
+            source .venv/bin/activate
+            gunicorn server:app & > /dev/null 2>&1
+            echo 'Started gunicorn server'
+        fi
+        ;;
+    stop)
+        if [ ! -f parent.pid ]; then
+            echo 'No gunicorn server running'
+        else
+            kill -9 `cat parent.pid`
+            echo 'Stopped gunicorn server'
+            rm parent.pid access.log
+        fi
+        ;;
+    *)
+        echo 'Invalid option. Enter start/stop'
+        ;;
+esac
